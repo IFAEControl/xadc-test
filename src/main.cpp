1fa1ce1e295beb102d72fde47fd2d47f1d064c7d
Otger <otger@ifae.es>
2018-07-19 09:45:25 +0200

    Changed init type from full to read only, added read of voltages

--- src/main.cpp
+++ src/main.cpp
@@ -4,9 +4,18 @@
 #include "xadc_core.h"
 
 int main() {
-    xadc_core x(true);
+    xadc_core x(false);
 
-    std::cout << x.get_value(xadc_param::temp) << std::endl;
-    std::cout << x.touch(xadc_param::temp) << std::endl;
-    x.set_threshold(xadc_alarm::temp, 62, 64);
+    std::cout << "Update Temp: " << x.touch(xadc_param::temp) << std::endl;
+    std::cout << "Update VccAux0: " << x.touch(xadc_param::vAux0) << std::endl;
+    std::cout << "Update VccAux: " << x.touch(xadc_param::vccAux) << std::endl;
+    std::cout << "Update VccBRam: " << x.touch(xadc_param::vccBRam) << std::endl;
+    std::cout << "Update VccInt: " << x.touch(xadc_param::vccInt) << std::endl;
+    std::cout << "Get Temp: " << x.get_value(xadc_param::temp) << std::endl;
+    std::cout << "Get VccAux0: " << x.get_value(xadc_param::vAux0) << std::endl;
+    std::cout << "Get VccAux: " << x.get_value(xadc_param::vccAux) << std::endl;
+    std::cout << "Get VccBRam: " << x.get_value(xadc_param::vccBRam) << std::endl;
+    std::cout << "Get VccInt: " << x.get_value(xadc_param::vccInt) << std::endl;
+    //x.set_threshold(xadc_alarm::temp, 62, 64);
+
 }
